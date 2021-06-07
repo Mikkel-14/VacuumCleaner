bce36daaadf2c240d98e83b2f64b3664fe29595b
Miguel Muñoz <jose.munoz01@epn.edu.ec>
2021-06-07 15:50:14 -0500

    Corrección error tipografico

--- src/enviroment.py
+++ src/enviroment.py
@@ -4,7 +4,7 @@ class Piso:
         self.nombre = nombre
     
     def __str__(self) -> str:
-        return "La valdosa {:} se encuentra en el estado {:}".format(self.nombre,self.estado)
+        return "La baldosa {:} se encuentra en el estado {:}".format(self.nombre,self.estado)
 
     #vamos a implementar un patron observador para los cambios de estado
     def setObservado(self, obs):
